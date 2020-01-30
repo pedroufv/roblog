ebbb8254f4610a176ebab9b7b3ce5d133a00000b
Pedro Augusto Mendes e Silva <pedro.e@ufv.br>
2020-01-30 17:19:59 -0300

    feat: create category crud by generator

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', 'IndexController@index')->name('index');
+
+Route::get('/categories/data', 'CategoryController@data')->name('categories.data');
+Route::get('/categories/{category}/delete', 'CategoryController@delete')->name('categories.delete');
+Route::resource('categories', 'CategoryController');
